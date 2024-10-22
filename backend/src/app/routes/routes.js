e8781a9ca71d621bc1c397fb093fa962f2daaf01
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-10-22 21:35:55 +0530

    changes2

--- backend/src/app/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { AuthRouter } from "../modules/Auth/auth.routes.js"
-import { PropertyRouter } from "../modules/property/property.routes.js"
-
-export const mainrouter = express.Router()
-
-mainrouter.use("/auth" ,AuthRouter)
-mainrouter.use("/recommend" ,PropertyRouter )
